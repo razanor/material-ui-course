269b1857117450a6cab79a935be7d4c36daead84
razanor <nazarii.repak@binary-studio.com>
2020-04-28 13:57:35 +0300

    Assets added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import Button from '@material-ui/core/Button';
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant='contained'>Click Me</Button>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div>Hello</div>;
+};
 
 export default App;
