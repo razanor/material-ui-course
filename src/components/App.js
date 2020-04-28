def34a922a589401e37431661bbe578107860597
razanor <nazarii.repak@binary-studio.com>
2020-04-28 14:18:28 +0300

    Basic AppBar setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './ui/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      Hello
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ui/Header.js
@@ -0,0 +1,29 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import React from 'react';
+
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const Header = () => {
+  return (
+    <ElevationScroll>
+      <AppBar>
+        <Toolbar>Arc Development</Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
+};
+
+export default Header;
