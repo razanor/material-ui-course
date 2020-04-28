5d6407d248f85c9c405c27954dd363f3a7a17070
razanor <nazarii.repak@binary-studio.com>
2020-04-28 16:32:36 +0300

    Add inline style with makeStyles

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,6 +2,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 
 function ElevationScroll(props) {
@@ -17,15 +18,26 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}));
+
 const Header = () => {
+  const classes = useStyles();
+
   return (
-    <ElevationScroll>
-      <AppBar>
-        <Toolbar>
-          <Typography variant='h3'>Arc Development</Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar>
+          <Toolbar>
+            <Typography variant='h3'>Arc Development</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
   );
 };
 
