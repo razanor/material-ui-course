2b659203a852cdae62c188a24f3ba326f6809673
razanor <nazarii.repak@binary-studio.com>
2020-04-29 19:45:53 +0300

    Header navigation setup

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,12 @@
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
@@ -25,8 +27,14 @@ const useStyles = makeStyles((theme) => ({
     ...theme.mixins.toolbar,
     marginBottom: '3em',
   },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
   logo: {
-    height: '7em',
+    height: '8em',
   },
   tabContainer: {
     marginLeft: 'auto',
@@ -36,24 +44,94 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 10,
     marginLeft: '25px',
   },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    height: '45px',
+  },
 }));
 
 const Header = () => {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_, value) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    const location = window.location.pathname;
+    const locationCondition = (path, tabIndex) =>
+      location === path && value !== tabIndex;
+
+    if (locationCondition('/', 0)) setValue(0);
+    else if (locationCondition('/services', 1)) setValue(1);
+    else if (locationCondition('/revolution', 2)) setValue(2);
+    else if (locationCondition('/about', 3)) setValue(3);
+    else if (locationCondition('/contact', 4)) setValue(4);
+    else if (locationCondition('/estimate', 5)) setValue(5);
+  }, [value]);
 
   return (
     <>
       <ElevationScroll>
         <AppBar>
           <Toolbar disableGutters>
-            <img className={classes.logo} src={logo} alt='company logo' />
-            <Tabs className={classes.tabContainer}>
-              <Tab className={classes.tab} label='Home' />
-              <Tab className={classes.tab} label='Services' />
-              <Tab className={classes.tab} label='The Revolution' />
-              <Tab className={classes.tab} label='About Us' />
-              <Tab className={classes.tab} label='Contanct Us' />
+            <Button
+              disableRipple
+              component={Link}
+              to='/'
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+            >
+              <img className={classes.logo} src={logo} alt='company logo' />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor='primary'
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/'
+                label='Home'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/services'
+                label='Services'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/revolution'
+                label='The Revolution'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/about'
+                label='About Us'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/contact'
+                label='Contanct Us'
+              />
             </Tabs>
+            <Button
+              variant='contained'
+              color='secondary'
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -23,5 +23,11 @@ export default createMuiTheme({
       fontWeight: 700,
       fontSize: '1rem',
     },
+    estimate: {
+      fontFamily: 'Pacifico',
+      fontSize: '1rem',
+      textTransform: 'none',
+      color: 'white',
+    },
   },
 });
